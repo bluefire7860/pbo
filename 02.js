5c99b484f2aa4aba82f37f04e0abec4cec76f063
bluefire7860 <bryan.louis.v@gmail.com>
2025-09-18 11:27:49 +0700

    Update 02.js

--- 02.js
+++ 02.js
@@ -1,1 +1,22 @@
+class Mobil {
+  // Kontruktor untuk inisialisasi objek
+  contruktor(merek, model, tahun) {
+    this.merek = merek; //Properti Objek
+    this.model = model;
+    this.tahun = tahun;
+  }
 
+  // Metode untuk menampilkan informasi mobik
+  displayinfo() {
+    console.log(`Mobil: ${this.merek} ${this.model}
+    (${this.tahun})`);
+  }
+
+  // Intasiasi objek dari kelas Mobil
+  const mobil1 = new Mobil('Toyota','Corolla',2020);  // Membuat Objek mobil1
+  const mobil2 = new Mobil('Honda','Civic',2021);     // Membuat Objek mobil2
+  // Memanggil metode untuk menampilkan informasi
+  mobil1.displayinfo();                               // Output: Mobil; Toyota Corolla (2020)
+  mobil2.displayinfo();                               // Output: Mobil; Honda Civic (2021)
+
+  
