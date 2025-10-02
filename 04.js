f7ea58fa24aa8f59ebc1779f60a611d83fd679c9
bluefire7860 <bryan.louis.v@gmail.com>
2025-10-02 13:53:15 +0700

    Update 04.js

--- 04.js
+++ 04.js
@@ -15,12 +15,21 @@ class Mahasiswa {
         if (nilaiBaru >= 0 && nilaiBaru <= 100) {
             this.#nilai = nilaiBaru;
         } else {
-            console.log("Nilai harus antara 0 dan 100");
+            console.log("❌ Nilai harus antara 0 dan 100");
         }
     }
 
-    displayInfo() {
-        console.log(`Nama: ${this.nama}, NIM: ${this.nim}, Nilai: ${this.#nilai}`);
+    displayNama() {
+        console.log(`================================`);
+        console.log(`Nama   : ${this.nama}`);
+    }
+    displayNIM() {
+        console.log(`NIM    : ${this.nim}`);
+    }
+    displayNilai() {
+        console.log(`Nilai  : ${this.#nilai}`);
+        console.log(`================================`);
+        console.log('')
     }
 }
 
@@ -28,18 +37,24 @@ const prodi1 = new Mahasiswa('Bryan', 2024001110018, 85);
 const prodi2 = new Mahasiswa('Egi', 2024001110078, 90);
 const prodi3 = new Mahasiswa('Danang', 2024001110095, 69);
 
-prodi1.displayInfo();             
+prodi1.displayNama(); 
+prodi1.displayNIM(); 
+prodi1.displayNilai(); 
 prodi1.nilai = 95;               
-console.log(prodi1.nilai);       
+console.log(`✅ Nilai terbaru: ${prodi1.nilai}\n`);       
 
 console.log("");
 
-prodi2.displayInfo();             
+prodi2.displayNama(); 
+prodi2.displayNIM(); 
+prodi2.displayNilai();          
 prodi2.nilai = 120;               
-console.log(prodi2.nilai);   
+console.log(`✅ Nilai terbaru: ${prodi2.nilai}\n`); 
 
 console.log("");
 
-prodi3.displayInfo();             
+prodi3.displayNama(); 
+prodi3.displayNIM(); 
+prodi3.displayNilai();
 prodi3.nilai = 50;               
-console.log(prodi3.nilai);
+console.log(`✅ Nilai terbaru: ${prodi3.nilai}\n`); 
